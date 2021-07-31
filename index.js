16195492b791860f9a4ec66521cb4a5f737db2bc
Zemke <florian@zemke.io>
2021-08-01 01:20:00 +0200

    Find latitudes

--- index.js
+++ index.js
@@ -6,32 +6,49 @@ const file = process.argv[2];
 console.log('reading', file);
 
 const stream = fs.createReadStream(file, { highWaterMark: 16 });
-let buffer;
-const lonBuffer = [];
-const lonRegex = new RegExp("lon=\"(.*?)\"");
-const latRegex = new RegExp("lat=\"(.*?)\"");
+let lonBuffer;
+let latBuffer;
+let lonBufferArr = [];
+let latBufferArr = [];
+let lonRegex = new RegExp("lon=\"(.*?)\"");
+let latRegex = new RegExp("lat=\"(.*?)\"");
 const interestingNumber = 6; // number of characters in regex not part of the capturing group
 
 stream.on('data', chunk => {
-  buffer += chunk.toString();
-  let match = buffer.match(lonRegex)
+  lonBuffer += chunk.toString();
+  latBuffer += chunk.toString();
+
+  const lonRes = bufferEm(lonBuffer, lonBufferArr, lonRegex);
+  lonBuffer = lonRes.buffer;
+  lonBufferArr = lonRes.bufferArr;
+
+  const latRes = bufferEm(latBuffer, latBufferArr, latRegex);
+  latBuffer = latRes.buffer;
+  latBufferArr = latRes.bufferArr;
+});
+
+function bufferEm(buffer, bufferArr, regex) {
+  let match = buffer.match(regex)
   while (match != null) {
     const bufferCloneSplit = buffer.slice().split('')
-    console.log('buffer before', buffer)
-    console.log('sub', bufferCloneSplit.join('').substr(0, match.index+match[1].length+interestingNumber))
-
     bufferCloneSplit.splice(0, match.index+match[1].length+interestingNumber-1)
     buffer = bufferCloneSplit.join('')
-    console.log('buffer after', buffer)
-    lonBuffer.push(match[1])
-    match = buffer.match(lonRegex)
+    bufferArr.push(match[1])
+    match = buffer.match(regex)
   }
-});
+  return { buffer, bufferArr }
+}
 
 stream.on('end', () => {
   console.log('END')
-  console.log(lonBuffer)
-  console.log(lonBuffer.length)
+
+  console.log("LON")
+  console.log(lonBufferArr)
+  console.log(lonBufferArr.length)
+
+  console.log("LAT")
+  console.log(lonBufferArr)
+  console.log(lonBufferArr.length)
 });
 
 stream.on('error', console.error);
