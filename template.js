4be336ec47509e2de46ba0537173347531b44eee
Zemke <florian@zemke.io>
2022-02-04 17:42:03 +0100

    Gotta be standalone

--- template.js
+++ template.js
@@ -1,6 +1,6 @@
 function start(name, stage) {
   const s = stage != null ? ` ${stage}` : '';
-  return `<?xml version='1.0' encoding='UTF-8'?>
+  return `<?xml version='1.0' encoding='UTF-8' standalone="yes"?>
 <gpx version="1.1" creator="https://gpx.zemke.io" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">
   <metadata>
     <name>${name}${s}</name>
